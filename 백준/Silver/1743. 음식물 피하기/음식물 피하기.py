18dc963d61d32d3fd806b250e46869915b7944d1
csh991414 <80662795+csh991414@users.noreply.github.com>
2024-02-07 14:28:45 +0900

    [Silver I] Title: 음식물 피하기, Time: 68 ms, Memory: 34088 KB -BaekjoonHub

--- 백준/Silver/1743. 음식물 피하기/음식물 피하기.py
+++ 백준/Silver/1743. 음식물 피하기/음식물 피하기.py
@@ -1,40 +1,46 @@
 import sys
 from collections import deque
-sys.setrecursionlimit(10**7)
 input = sys.stdin.readline
+
+
 n,m,k = map(int,input().split())
-
-graph = [[0] * (m) for _ in range (n)]
-
-for i in range(k):
-    a,b= map(int,input().split())
-    graph[a-1][b-1] = 1
-    
-count =0
-arr=[]
+graph = [ [0] *m for _ in range(n)]
 dx = [0,0,1,-1]
 dy = [1,-1,0,0]
 
 
-def dfs(x,y):
-    global count
-    if x<0  or y<0 or x>=n or y>=m:
-        return False
+for i in range(k):
+    a,b = map(int,input().split())
+    graph[a-1][b-1] = 1
 
-    if graph[x][y] ==1:
-        graph[x][y]=0
-        count+=1
+def bfs(x,y):
+
+    q= deque()
+    q.append((x,y))
+    graph[x][y] =0
+    cnt =0
+    
+    while q:
+        a,b = q.popleft()
+        cnt+=1
         for i in range(4):
-            nx= x+dx[i]
-            ny= y+dy[i]
-            dfs(nx,ny)
-        return True
-    return False
+            nx = dx[i] + a
+            ny = dy[i] + b
 
+            if 0<= nx < n and 0<= ny < m and graph[nx][ny] ==1:
+                q.append((nx,ny))
+                graph[nx][ny] = 0
+                
+
+
+    
+    return cnt
+
+
+max_count =0
 for i in range(n):
-  for j in range(m):
-    if dfs(i, j) == True:
-      arr.append(count)
-      count = 0    
+    for j in range(m):
+        if graph[i][j] ==1:
+            max_count = max(max_count,bfs(i,j))
 
-print(max(arr))
+print(max_count)
